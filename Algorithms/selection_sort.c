8fff7804d16e5f8dabd636d01999db507140dc4b
P Niranjan Roy <niranjan275865@gmail.com>
2026-05-23 22:40:13 +0530

    Create selection_sort.c
    Added Selection Sort algorithm in C

---
+++ Algorithms/selection_sort.c
@@ -0,0 +1,9 @@
+Step 1: Start
+Step 2: Input array A of size n
+Step 3: For i = 0 to n-2
+Step 4: Set min = i
+Step 5: For j = i+1 to n-1
+Step 6: If A[j] < A[min], set min = j
+Step 7: Swap A[i] and A[min]
+Step 8: Repeat
+Step 9: Stop
