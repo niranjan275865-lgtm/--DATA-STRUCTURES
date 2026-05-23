1ba650f76dab861ac778b001b7e62dd952c06d66
P Niranjan Roy <niranjan275865@gmail.com>
2026-05-23 22:36:31 +0530

    Added Bubble Sort algorithm in C

---
+++ Algorithms/bubble_sort.c
@@ -0,0 +1,7 @@
+Step 1: Start
+Step 2: Input array A of size n
+Step 3: For i = 0 to n-1
+Step 4: For j = 0 to n-1-i
+Step 5: If A[j] > A[j+1], swap them
+Step 6: Repeat until array is sorted
+Step 7: Stop
