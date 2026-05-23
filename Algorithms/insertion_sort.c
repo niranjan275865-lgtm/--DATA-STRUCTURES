c7cc9dd52e1ce5cf70a12a2352abea9515715093
P Niranjan Roy <niranjan275865@gmail.com>
2026-05-23 22:41:41 +0530

    Create insertion_sort.c
    Added Insertion Sort algorithm in C

---
+++ Algorithms/insertion_sort.c
@@ -0,0 +1,8 @@
+Step 1: Start
+Step 2: Input array A of size n
+Step 3: For i = 1 to n-1
+Step 4: Set key = A[i]
+Step 5: Compare key with previous elements
+Step 6: Shift larger elements to right
+Step 7: Insert key in correct position
+Step 8: Stop
