ac5ecceacc8789b3cc8e00d907dc59dfcc689f4d
P Niranjan Roy <niranjan275865@gmail.com>
2026-05-24 06:12:21 +0530

    Create binary_search.c
    Added Binary Search algorithm in C

---
+++ Algorithms/binary_search.c
@@ -0,0 +1,8 @@
+Step 1: Start
+Step 2: Input sorted array
+Step 3: Set low and high
+Step 4: Find middle element
+Step 5: Compare item with middle
+Step 6: Move left or right
+Step 7: Repeat until found
+Step 8: Stop
