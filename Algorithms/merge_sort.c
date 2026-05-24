3567f0e2dd480840393045ad0c40656b389ca959
P Niranjan Roy <niranjan275865@gmail.com>
2026-05-24 06:11:28 +0530

    Create merge_sort.c
    Added Merge Sort algorithm in C

---
+++ Algorithms/merge_sort.c
@@ -0,0 +1,7 @@
+Step 1: Start
+Step 2: Divide array into two halves
+Step 3: Apply merge sort on left half
+Step 4: Apply merge sort on right half
+Step 5: Merge both sorted halves
+Step 6: Repeat until fully sorted
+Step 7: Stop
